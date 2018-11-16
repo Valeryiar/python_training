d84e43f07bc4db605cb5adaa7dc169fa18879f99
Valeryiar <43443600+Valeryiar@users.noreply.github.com>
2018-11-16 16:17:23 +0300

    11

--- conftest.py
+++ conftest.py
@@ -1,6 +1,12 @@
 import pytest
 from fixture.application import Application
-
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+cap = DesiredCapabilities().FIREFOX
+cap["marionette"] = False
+browser = webdriver.Firefox(capabilities=cap, executable_path="C:\\path\\to\\geckodriver.exe")
+browser.get('http://google.com/')
+browser.quit()
 
 @pytest.fixture(scope="session")
 def app(request):
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,13 +3,6 @@
 from model.group import Group
 
 
-from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-cap = DesiredCapabilities().FIREFOX
-cap["marionette"] = False
-browser = webdriver.Firefox(capabilities=cap, executable_path="C:\\path\\to\\geckodriver.exe")
-browser.get('http://google.com/')
-browser.quit()
 
 
 
