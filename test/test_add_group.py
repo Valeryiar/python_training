737e14d283dc48b096d073557ca3766445ee6050
Valeryiar <43443600+Valeryiar@users.noreply.github.com>
2018-11-16 15:10:07 +0300

    9

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -22,12 +22,12 @@ def app(request):
 
 def test_add_group(app):
     app.session.login( username="admin", password="secret")
-    app.create_group(Group(name="dbs", header="sergseg", footer="eshaseh"))
+    app.group.create(Group(name="dbs", header="sergseg", footer="eshaseh"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login( username="admin", password="secret")
-    app.create_group( Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
 
