c4e92ee783134bb22a38b6cb38f912cf99bef2d8
Valeryiar <43443600+Valeryiar@users.noreply.github.com>
2018-11-21 18:42:01 +0300

    29

--- data/add_group.py
+++ data/add_group.py
@@ -3,8 +3,7 @@ import random
 import string
 
 constant = [
-    Group (name="name1", header="header1", footer = "footer1"),
-    Group (name="name2", header="header2", footer = "footer2"),
+    Group (name="name1", header="header1", footer = "footer1")
 ]
 
 def random_string(prefix, maxlen):
