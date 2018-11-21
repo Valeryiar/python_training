3bab52742043dedbcf5083f48c8a916dbe418d49
Valeryiar <43443600+Valeryiar@users.noreply.github.com>
2018-11-21 18:38:41 +0300

    29

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,19 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-import random
-import string
-
+from data.add_group import constant as testdata
 from model.group import Group
-
-def random_string(prefix, maxlen):
-    symbols = string.digits+""*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Group(name="", header="", footer="")] #+ [
-  #  Group(name=random_string("name", 10), header=random_string("header", 10), footer=random_string("footer", 10))
-  #  for i in range(3)
-#]
 
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
